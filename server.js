587d4b63738e50eacae0fa2c8a4abd8fbc983cef
Horace <49237174+MHieu128@users.noreply.github.com>
2024-04-03 13:32:38 +0700

    Update format code

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
 
 const PORT = process.env.PORT || process.env.API_PORT;
 const app = express();
@@ -14,17 +14,18 @@ app.use(express.json());
 app.use(cors());
 
 //register the routers
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 const server = http.createServer(app);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  server.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`server is running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Cannot connect to database");
+    console.error(err);
   });
-})
-.catch(err => {
-  console.log('Cannot connect to database');
-  console.error(err);
-})
